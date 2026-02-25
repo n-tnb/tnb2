975b4b82e243d4e36cae4157c572b3dacc4258f1
rafavd <rafarovd@gmail.com>
2026-02-25 00:19:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const { connect } = require("puppeteer-real-browser");
+//const { GhostCursor } = require('ghost-cursor');
+
+const run = async () => {
+  const { page, browser } = await connect({
+    args: ["--start-maximized"],
+    turnstile: true,
+    headless: false,
+    // disableXvfb: true,
+    proxy: {
+      host: "104.234.127.10",
+      port: 47568,
+      username: "BHVTZ9TQ",
+      password: "EBD8SU4H",
+    },
+    customConfig: {},
+    connectOption: {
+      defaultViewport: null,
+    },
+    plugins: [],
+  });
+
+  await page.goto("https://thenanobutton.com/u5Xtxg?unit=dollars", {
+    waitUntil: "networkidle2",
+  });
+
+  await page.evaluate(() => {
+    document.body.style.zoom = "50%";
+  });
+
+  await new Promise((r) => setTimeout(r, 10000));
+
+  await page.waitForSelector("circle");
+
+  let count = 0;
+  while (count < 100) {
+    // await page.realCursor.click("circle")
+
+    // mouse click
+    const circle = await page.$("circle");
+    const boundingBox = await circle.boundingBox();
+    const x = boundingBox.x + boundingBox.width / 2;
+    const y = boundingBox.y + boundingBox.height / 2;
+    await page.mouse.click(x, y);
+
+    await new Promise((r) => setTimeout(r, 100));
+    count++;
+  }
+
+  await new Promise((r) => setTimeout(r, 5000));
+
+  let token = null;
+  let startDate = Date.now();
+  while (!token && Date.now() - startDate < 30000) {
+    token = await page.evaluate(() => {
+      try {
+        let item = document.querySelector(
+          '[name="cf-turnstile-response"]',
+        ).value;
+        return item && item.length > 20 ? item : null;
+      } catch (e) {
+        return null;
+      }
+    });
+    await new Promise((r) => setTimeout(r, 1000));
+  }
+
+  await new Promise((r) => setTimeout(r, 5000));
+
+  console.log("Token:", token);
+
+  await page.screenshot({ path: "screen.png" });
+
+  //   await browser.close();
+};
+
+run();
